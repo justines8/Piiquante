c474ed64e7e35add92c5dcab0780b7dbe3484fca
justines8 <119518688+justines8@users.noreply.github.com>
2023-04-21 12:12:17 +0200

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+/* importing the Express.js framework and configuring it to use JSON as the data 
+format for incoming requests.*/
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const helmet = require("helmet");
+
+/* The `dotenv.config()` method is called to load the environment
+variables from the `.env` file into the `process.env` object.*/
+const dotenv = require("dotenv");
+dotenv.config();
+
+/* This code is connecting the Node.js application to a MongoDB database using the Mongoose library.*/
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+mongoose.connect(`mongodb+srv://${process.env.DB_MAIL}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}${process.env.DB_TEST}`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const userRoutes = require("./routes/user");
+
+/* This code is setting up CORS (Cross-Origin Resource Sharing) headers for the Express.js app. CORS is
+a security feature implemented by web browsers that restricts web pages from making requests to a
+different domain than the one that served the page.*/
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_CORS_URL);
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(helmet());
+
+app.use("/api/auth/", userRoutes);
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,50 @@
+const http = require('http');
+
+const app = require('./app');
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/*The function handles errors that occur during server listening.*/
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+    default:
+      throw error;
+  }
+};
+
+/* creating a new HTTP server instance using the `http` module and passing in the `app` 
+module as a callback function to handle incoming requests.*/
+const server = http.createServer(app);
+
+/*setting up event listeners for the HTTP server instance created using `http.createServer(app)`.*/
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+/*starting the HTTP server and causing it to listen for incoming requests on the specified port.*/
+server.listen(port);
