14a076aa1349d2d762ec365b2a819d6201c4fcad
justines8 <119518688+justines8@users.noreply.github.com>
2023-06-13 20:08:39 +0200

    Modif avant soutenance

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,25 +7,24 @@ const userSchema = mongoose.Schema({
         type: String, 
         required: true, 
         unique: true, },
-        /*validate: {
+        validate: {
             validator: function(value) {
               const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
               return emailRegex.test(value);
             },
             message: 'Format de l\'adresse e-mail non valide'
-          }},*/
+          },
     password: { 
       type: String, 
       required: true,
-      /*minlength: 4,
+      minlength: 4,
       validate: {
         validator: function(value) {
           const passwordRegex = /^(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
           return passwordRegex.test(value);
         },
         message: 'Le mot de passe doit comporter au moins 4 caractères, 1 chiffre et 1 caractère spécial'
-      } */
-    },
+      } }
 });
 
 userSchema.plugin(uniqueValidator);
